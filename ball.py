5b422e80a6917490f7e94e23f3859da1c50b8f90
tol2000 <tolicheg@gmail.com>
2020-12-29 21:52:57 +0200

    add moving ball )

--- ball.py
+++ ball.py
@@ -1,17 +1,43 @@
+from random import randint
+
+from pygame import Surface
 from pygame.draw import circle
+
+from mycolors import MyColors
 
 
 class Ball:
 
-    def __init__(self, screen, color, x, y, r):
-        self.x = x
-        self.y = y
-        self.r = r
+    MIN_RADIUS = 40
+    MAX_RADIUS = 60
+
+    def __init__(self, screen: Surface, color):
         self.screen = screen
         self.color = color
+
+        self.x = randint(self.MAX_RADIUS, self.screen.get_width()-self.MAX_RADIUS)
+        self.y = randint(self.MAX_RADIUS, self.screen.get_height()-self.MAX_RADIUS)
+        self.r = randint(self.MIN_RADIUS, self.MAX_RADIUS)
+
+        self.vx = randint(-5, 5)
+        self.vy = randint(-5, 5)
+
+        self.paint(color=self.color)
 
     def point_inside_ball(self, point_x, point_y):
         return (point_x - self.x)**2 + (point_y - self.y)**2 <= self.r**2
 
-    def paint(self):
-        circle(self.screen, self.color, (self.x, self.y), self.r)
+    def paint(self, color):
+        circle(self.screen, color, (self.x, self.y), self.r)
+
+    def repaint(self):
+        self.paint(color=MyColors.BLACK)
+        self.x += self.vx
+        self.y += self.vy
+        if self.x < self.r or self.x > self.screen.get_width()-self.r:
+            self.vx = -self.vx
+            self.x += self.vx
+        if self.y < self.r or self.y > self.screen.get_height()-self.r:
+            self.vy = -self.vy
+            self.y += self.vy
+        self.paint(color=self.color)
--- catch_ball.py
+++ catch_ball.py
@@ -4,49 +4,49 @@ import pygame
 from pygame.draw import circle
 
 from ball import Ball
+from mycolors import MyColors
 
 
 class CatchBall:
 
-    RED = (255, 0, 0)
-    BLUE = (0, 0, 255)
-    YELLOW = (255, 255, 0)
-    GREEN = (0, 255, 0)
-    MAGENTA = (255, 0, 255)
-    CYAN = (0, 255, 255)
-    BLACK = (0, 0, 0)
-    COLORS = [BLUE, YELLOW, MAGENTA, CYAN]
+    FPS: int = 150
+    COLORS = [MyColors.BLUE, MyColors.YELLOW, MyColors.MAGENTA, MyColors.CYAN]
+    MIN_SPEED = 50
+    MAX_SPEED = FPS
+    SPEED_INC = 5
 
-    def __init__(self, balls_per_second: float):
+    current_speed = MAX_SPEED
+
+    def __init__(self):
         self.ball = None
         self.balls = 0
         self.hits = 0
         self.missed = 0
 
         self.frames = 0
+        self.frames_between_move = 0
 
         pygame.init()
 
-        self.FPS: int = 30
         self.screen = pygame.display.set_mode((1200, 900))
 
-        self.FRAMES_BETWEEN_BALLS = self.FPS / balls_per_second
+        self.recalculate_speed()
 
         pygame.display.update()
         self.clock = pygame.time.Clock()
         self.finished = False
 
+        self.init_balls()
+
     '''
     Creates and draws new ball
     '''
-    def new_ball(self):
-        x = randint(100, 1100)
-        y = randint(100, 900)
-        r = randint(10, 100)
+    def init_balls(self):
+        self.screen.fill(MyColors.BLACK)
         color = self.COLORS[randint(0, len(self.COLORS)-1)]
-        self.ball = Ball(self.screen, color, x, y, r)
-        self.ball.paint()
+        self.ball = Ball(self.screen, color)
         self.balls += 1
+        self.frames = 0
 
     def key_down_event_handler(self, event):
         if event.key == pygame.K_q:
@@ -56,12 +56,14 @@ class CatchBall:
         if event.button == 1:
             if self.ball:
                 if self.ball.point_inside_ball(event.pos[0], event.pos[1]):
-                    circle(self.screen, self.GREEN, event.pos, 20)
+                    circle(self.screen, MyColors.GREEN, event.pos, 5)
                     self.hits += 1
+                    self.increase_speed()
+                    self.init_balls()
                 else:
-                    circle(self.screen, self.RED, event.pos, 20)
+                    circle(self.screen, MyColors.RED, event.pos, 5)
                     self.missed += 1
-                print(f'Balls: {self.balls}, Hits: {self.hits}, Missed: {self.missed}')
+                    self.decrease_speed()
 
     def run(self):
         while not self.finished:
@@ -73,14 +75,29 @@ class CatchBall:
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     self.mouse_down_event_handler(event)
 
-            if self.frames < self.FRAMES_BETWEEN_BALLS:
+            if self.frames < self.frames_between_move:
                 self.frames += 1
             else:
-                self.screen.fill(self.BLACK)
-                self.new_ball()
+                self.ball.repaint()
                 self.frames = 0
 
+            pygame.display.set_caption(
+                f'Speed: {self.current_speed}, Balls: {self.balls}, Hits: {self.hits}, Missed: {self.missed}'
+            )
             pygame.display.update()
             self.clock.tick(self.FPS)
 
         pygame.quit()
+
+    def recalculate_speed(self):
+        self.frames_between_move = self.FPS / self.current_speed
+
+    def increase_speed(self):
+        if self.current_speed <= self.MAX_SPEED-self.SPEED_INC:
+            self.current_speed += self.SPEED_INC
+            self.recalculate_speed()
+
+    def decrease_speed(self):
+        if self.current_speed >= self.MIN_SPEED+self.SPEED_INC:
+            self.current_speed -= self.SPEED_INC
+            self.recalculate_speed()
